ef336e141cb8e0b7507554d55315f05cf76dadfa
kds1024 <kobayashidenshisetubi@gmail.com>
2020-05-25 18:08:07 +0900

    AsyncTaskで実装する

--- app/src/main/java/com/example/threadtest04/MainActivity.kt
+++ app/src/main/java/com/example/threadtest04/MainActivity.kt
@@ -1,77 +1,61 @@
 package com.example.threadtest04
 
 import android.os.*
+import android.os.AsyncTask.execute
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    // 1.別スレッド(HandlerThread)を生成
-    val simplethread = HandlerThread("simplethread")
-    var simplehandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mText = findViewById<TextView>(R.id.text)
+        val text = findViewById<TextView>(R.id.text)
 
-        // 別スレッドからMainActivityにメッセージを送信するためのハンドラー
-        val mhandler : Handler = object : Handler(Looper.getMainLooper()) {
-            override fun handleMessage(msg: Message) {
-                when (msg.what){
-                    1 -> {
-                        Log.d("kd>", "${msg.obj}")
-                        mText.setText("${msg.obj}")
-                    }
-                    2 -> {
-                        Log.d("kd>", "${msg.obj}")
-                        mText.setText("${msg.obj}")
-                        simplethread.quit()
-                    }
+        AsyncTaskAAA().also { it.textAAA = text }.execute(1)
 
-                }
-            }
-        }
+    }
+}
 
-        // 2.別スレッド開始
-        simplethread.start()
+class AsyncTaskAAA : AsyncTask<Int, Int, Int>() {
+    var textAAA: TextView? = null
 
-        // 3.Handlerインスタンスを生成(2.で作成したthreadのLooperインスタンスを引数指定)
-        // スレッドを#start()しないとlooperは使えない（初期化されずlooperがnullのままで例外がスローされる)
-        // 別スレッドで処理させたいタスクをpostするハンドラー
-        simplehandler = Handler(simplethread.looper)
-
-        //Log.d("kd>", "simplelooper ${simplethread.looper}")
-        //Log.d("kd>", "simplehandler ${simplehandler}")
-
-        // 4. 3で作成したHandlerインスタンスを使用(Handler#post 等)
-        // Runnableをスレッドにpostするとこのタスクをスレッドで実行してくれる
-        // Runnableには引数も返り値もない
-        val r = Runnable {
-            for (i in 1..1000){
-                Log.d("kd>", "タスク開始 ${HandlerThread.currentThread()}")
-                HandlerThread.sleep(1000)
-                Log.d("kd>", "タスク完了 ${HandlerThread.currentThread()}")
-
-                // スレッド内からMainActivityにメッセージを送信する
-                // これで、スレッドの処理が完了したことがMainActivityに通知できる
-                val msg = mhandler.obtainMessage(1, i)
-                mhandler.sendMessage(msg)
-            }
-            // スレッド内からMainActivityにメッセージを送信する
-            // これで、スレッドの処理が完了したことがMainActivityに通知できる
-            val msg = mhandler.obtainMessage(2, "complete")
-            mhandler.sendMessage(msg)
-        }
-        // simplethreadのハンドラーにRunnableをポストして別スレッドでタスクを実行する
-        simplehandler.post(r)
+    override fun onPreExecute() {
+        super.onPreExecute()
     }
 
-    override fun onDestroy() {
-        simplethread.quit()
-        simplehandler = Handler(Looper.getMainLooper())
-        super.onDestroy()
+    override fun doInBackground(vararg params: Int?): Int {
+        //TODO("Not yet implemented")
+        var count = 0
+        // nullではないことを確認してからcountをnon-null化
+        // publishProgress(count)のcountがプリミティブ型なので変換する
+        if (params[0] != null) {
+            count = params[0]!!
+        }
+        for (i in 1..100) {
+            Log.d("kd>", "${count}")
+            publishProgress(count)
+            Thread.sleep(1000)
+            count += 1
+        }
+        return 0
+    }
+
+    override fun onProgressUpdate(vararg values: Int?) {
+        super.onProgressUpdate(*values)
+        //Log.d("kd>", "${values[0]}")
+
+        textAAA?.setText("${values[0]}")
+
+    }
+
+    override fun onPostExecute(result: Int?) {
+        super.onPostExecute(result)
+        val str = "Complete"
+        textAAA?.setText(str)
+
     }
 
 }
